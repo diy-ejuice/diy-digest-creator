217ae36d3c6e29c12449e25ddac4ad0ca0ffe545
ayan4m1 <andrew@bulletlogic.com>
2020-04-11 19:44:24 -0400

    fix config commas

--- src/modules/config.js
+++ src/modules/config.js
@@ -2,9 +2,9 @@ import 'dotenv/config';
 
 export const logging = {
   level: process.env.DIY_LOG_LEVEL || 'info',
-  timestampFormat: process.env.DIY_LOG_TIME_FMT,
+  timestampFormat: process.env.DIY_LOG_TIME_FMT
 };
 
 export default {
-  logging,
+  logging
 };
